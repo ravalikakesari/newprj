8e590d758382116b99e24d72a179cbff93f591ac
ravalika <ravalika@21.com>
2020-07-13 11:51:35 +0530

    loop.js

---
+++ loop.js
@@ -0,0 +1,15 @@
+//var _ = require("lodash");
+var people = [
+    {name: "frank",age: 20, gender: 'male'},
+    {name: "pinky",age: 22, gender: 'female'}
+];
+var susan;
+for (var i = 0; i < people.length; i++)
+{
+    var person = people[i];
+    if(person.name == "susan"){nod
+        susan = person;
+    }
+}
+console.log(susan);
+    
