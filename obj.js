584aebca1b5558e463870f517e20bef11f6f3fe7
ravalika <ravalika@21.com>
2020-07-13 12:11:27 +0530

    obj.js

---
+++ obj.js
@@ -0,0 +1,6 @@
+let user = {     // an object
+    name: "John",  // by key "name" store value "John"
+    age: 30        // by key "age" store value 30
+  };
+  console.log(user.name);
+  console.log(user.age);
